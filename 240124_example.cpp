2b371f0a527b0cf09ce05086f4f67e5e4e20c0ec
root <root@DESKTOP-RSG0KCC>
2024-01-24 04:34:06 +0900

    v1.2 if absolute number

--- 240124_example.cpp
+++ 240124_example.cpp
@@ -1,14 +1,22 @@
+// if ~ absolute num.
+
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    int num;
 
-int main(){
-    
-    int x;
-    x = 0;    
-    cout << ++x << ++x << ++x << endl;
-    int y = 0;
-    cout << --y;
+    cout << "Input integer: ";
+    cin >> num;
+
+    if(num < 0)
+    {
+        num = -num;
+    }
+
+    cout << "Absolute value of the number : " << num;
     return 0;
+
 
 }
