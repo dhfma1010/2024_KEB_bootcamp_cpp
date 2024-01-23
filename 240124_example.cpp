7f304e454172888cd11acfb4bd8970d6f702bbc7
root <root@DESKTOP-RSG0KCC>
2024-01-24 02:06:33 +0900

    v0.8

--- 240124_example.cpp
+++ 240124_example.cpp
@@ -2,14 +2,22 @@
 using namespace std;
 
 int main(){
-    unsigned int givenInt, firstDigit;
+    unsigned int givenInt, firstDigit, secondDigit, thirdDigit, forthDigit;
 
     cout << "Input a positive integer: ";
     cin >> givenInt;
 
     firstDigit = givenInt % 10;
+    secondDigit = givenInt % 100 / 10;
+    thirdDigit = givenInt % 1000 / 100;
+    forthDigit = givenInt % 10000 / 1000;
 
-    cout << "Input integer" <<givenInt << endl;
+    cout << "Input integer " <<givenInt << endl;
     cout << "Extracted first digit. " << firstDigit  << endl;
+    cout << "Extracted second digit. " << secondDigit  << endl;
+    cout << "Extracted third digit. " << thirdDigit  << endl;
+    cout << "Extracted forth digit. " << forthDigit  << endl;
+
+
     return 0;
 }
