b409dc9d521ec2af6506defa87e82ceb474212b0
root <root@DESKTOP-RSG0KCC>
2024-01-24 01:15:54 +0900

    v0.7

--- 240124_example.cpp
+++ 240124_example.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main(){
-    // variable declaration
-    double number;
-    int intPart;
-    double fractPart;
-    // input
-    cout << "Input a floating-point number: ";
-    cin >> number;
-    // processing
-    intPart = static_cast<int>(number);
-    fractPart = number - intPart;
-    // output
-    cout << fixed << showpoint << setprecision(2);
-    cout << "The original number: " << number << endl;
-    cout << "The integral part: " << intPart << endl;
-    cout << "The fractional part:" << fractPart;
+    unsigned int givenInt, firstDigit;
+
+    cout << "Input a positive integer: ";
+    cin >> givenInt;
+
+    firstDigit = givenInt % 10;
+
+    cout << "Input integer" <<givenInt << endl;
+    cout << "Extracted first digit. " << firstDigit  << endl;
     return 0;
-
-
 }
